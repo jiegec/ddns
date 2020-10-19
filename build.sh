43cee1d5a95b8c83b2bcab7043db5c6b82aeccf2
Jiajie Chen <c@jia.je>
2020-10-19 10:43:03 +0800

    Add binary for mipsle

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o ddns-darwin-amd64
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ddns-linux-amd64
 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o ddns-linux-arm64
 GOOS=linux GOARCH=ppc64le go build -ldflags="-s -w" -o ddns-linux-ppc64le
+GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o ddns-linux-mipsle
