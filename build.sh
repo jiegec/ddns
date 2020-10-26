117a9d418ed7c9b4d4dc28c29ac4bd318b7d0fd0
jiegec <noc@jiegec.ac.cn>
2020-10-26 15:23:08 +0800

    Disable cgo

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 go build -o ddns
+export CGO_ENABLED=0
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ddns-windows-amd64
 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o ddns-darwin-amd64
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ddns-linux-amd64
